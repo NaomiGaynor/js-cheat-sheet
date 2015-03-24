2bdb3caf246180ed88f73cc9a501411fa67fc2d3
Naomi Gaynor <naomi.gaynor@gmail.com>
2015-03-24 11:37:47 +0000

    adds comments

--- js/fadescroll.js
+++ js/fadescroll.js
@@ -1,11 +1,21 @@
-var info = document.getElementById("info"), 
-	speed = 300, 
+/***********************************
+fadescroll.js
+function that will make elememnts fade as user scrolls down page.
+
+***********************************/
+
+
+//Put ID of element in parenthesis on line 9 
+var elementId = document.getElementById(""), 
+	//change variable on line 11 to change the speed at which the elemennt will fade
+	fadespeed = 300, 
 	y, 
+	//checks compatibility of browser
 	supportPageOffset = window.pageXOffset != undefined,
 	isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-
+//uses appropriate variable depending on compatibility of browser.
 window.onscroll = function(y){
 	y= supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrolTop;
-
-	info.style.opacity = 1-(y/speed);
+	//changes the opacity of element as user scrolls 
+	elementId.style.opacity = 1-(y/fadespeed);
 }
