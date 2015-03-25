c3a86e502a8901a15cf6a841d2926fae331b7842
Naomi Gaynor <naomi.gaynor@gmail.com>
2015-03-25 21:23:01 +0000

    comments function for clarity

--- js/parallax-scroll.js
+++ js/parallax-scroll.js
@@ -2,29 +2,27 @@
 skeleton of a simple paralax scroll 
 
 *******************************/
-
-var screen1 = document.getElementById(""),
-    title = document.getElementById("title"),
-    lightbulb = document.getElementById("lightbulb"),
-    slow = 4,
-    fast = 0.5,
-    fadeSpeed = 300,
+//insert first id from html 
+var item1 = document.getElementById(""),
+    item2 = document.getElementById(""),
+    
+    speed = 4,
+    
+    
     y,
-    // Applies a 25% movement in the OPPOSITE direction to the scroll so as to simulate slow movement up the page.
-        // inverted it will speed up the movement up of an element by 4
+   //checks compatability of browser
     supportPageOffset = window.pageXOffset !== undefined,
-        // true or false, browser does not have property .pageXOffset
-        // if false (and property is undefined then the browser is probably ie8)
+        
+        // if false is CSS1Compat will work  
     isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-        // true or false, the document is rendered in "Standards mode"
-        // if false (and is "BackCompat"), document is rendered in Quirks mode and is probably old.
+        
         
 window.onscroll = function() {
+    //defines y depending on browser compatability 
     y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-       // defines y as pageYOffset if modern browser
-    screen1.style.backgroundPosition = "0px " + (y / slow) + "px";
-        // Move elements background position not element itself
-    title.style.opacity = "1" - (y / fadeSpeed);
-    title.style.transform = "translate(0px," + (y / -slow) + "px)";
-    lightbulb.style.transform = "translate(0px," + (y / -fast) + "px)";
+      //moves position of item1 at a slower speed than scroll 
+    item1.style.backgroundPosition = "0px " + (y / speed) + "px";
+      //moves position of item2 at faster speed than scroll  
+    item2.style.transform = "translate(0px," + (y / -speed) + "px)";
+    
 }
