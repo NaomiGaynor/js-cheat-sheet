cdaba2574ec085b37049f4bafbed9f52debb8ff1
Naomi Gaynor <naomi.gaynor@gmail.com>
2015-03-26 13:45:57 +0000

    adds comments to function

--- js/toggle-visability.js
+++ js/toggle-visability.js
@@ -5,6 +5,8 @@ useful function for pop-up boxes and hiding and displaying elements on the page.
 ******************************************/
 
 function toggle_visability(id){
+	//retrieves element from DOM that matches id.
 	var toggle = document.getElementById(id);
+	// tertiary operator that will toggle between  visable and invisable depending on the current state on the DOM.
 	toggle.style.dislay == 'block' ? toggle.style.display = 'none': toggle.style.display = 'block';
 }
